e2086c34c5deba02f14ad417fc70f5061df88018
majednnnn9 <tt292665@gmail.com>
2024-04-20 13:51:59 +0100

    add majed.txt file

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+
+
+app.get('/user', (req, res) => {
+    res.json({
+        "message": "succesful",
+        "data": [
+            {
+                "title": "book 1",
+                "description": "description 1"
+            },
+            {
+                "title": "book 2",
+                "description": "description 2"
+            }
+        ]
+    })
+})
+
+
+const PORT = 8080 || process.env.PORT;
+app.listen(PORT, () => {
+    console.log("runing is server is port " + PORT)
+})
