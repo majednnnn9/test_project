41e4ed37bb10b4319b35ba768ef69a9c10d56e26
majednnnn9 <tt292665@gmail.com>
2024-04-22 00:19:55 +0100

    add value database

--- config/db.js
+++ config/db.js
@@ -1,20 +1,25 @@
 const mysql = require('mysql');
 
 // إعداد معلومات الاتصال
+// const connection = mysql.createConnection({
+//   host: 'boyjhxrxwfbza4rdyzm7-mysql.services.clever-cloud.com', // اسم خادم قاعدة البيانات
+//   user: 'uqd5wvj9z0in3ysf', // اسم المستخدم
+//   password: 'V1oRdmlpVok7s2qg2BZp', // كلمة المرور
+//   database: 'boyjhxrxwfbza4rdyzm7' // اسم قاعدة البيانات
+// });
 const connection = mysql.createConnection({
-  host: 'boyjhxrxwfbza4rdyzm7-mysql.services.clever-cloud.com', // اسم خادم قاعدة البيانات
-  user: 'uqd5wvj9z0in3ysf', // اسم المستخدم
-  password: 'V1oRdmlpVok7s2qg2BZp', // كلمة المرور
-  database: 'boyjhxrxwfbza4rdyzm7' // اسم قاعدة البيانات
+    host: process.env.host, // اسم خادم قاعدة البيانات
+    user: process.env.host, // اسم المستخدم
+    password: process.env.password, // كلمة المرور
+    database: process.env.database // اسم قاعدة البيانات
 });
-
 // إنشاء الاتصال
 connection.connect((error) => {
-  if (error) {
-    console.error('حدث خطأ في الاتصال بقاعدة البيانات: ' + error.stack);
-    return;
-  }
-  console.log('تم الاتصال بنجاح بقاعدة البيانات!');
+    if (error) {
+        console.error('حدث خطأ في الاتصال بقاعدة البيانات: ' + error.stack);
+        return;
+    }
+    console.log('تم الاتصال بنجاح بقاعدة البيانات!');
 });
 
 // قم بتصدير الاتصال لاستخدامه في ملفات أخرى
